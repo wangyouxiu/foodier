c35cfc774b1b405906ded183a020c8fe0a43c696
wangyue <269773646@qq.com>
2020-03-17 22:32:59 +0800

    feat:修改cors设置，解决跨域问题

--- foodier-dev-api/src/main/java/com/imooc/config/CorsConfig.java
+++ foodier-dev-api/src/main/java/com/imooc/config/CorsConfig.java
@@ -24,7 +24,8 @@ public class CorsConfig {
         //1.添加cors配置信息
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedOrigin("http://47.106.230.215:8080");
+        config.addAllowedOrigin("http://47.106.230.215:90");
+        config.addAllowedOrigin("*");
         //设置是否发送cookie信息
         config.setAllowCredentials(true);
         //设置允许请求的方式
